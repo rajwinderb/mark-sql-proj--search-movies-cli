94de8cfc43723114d66da92d2eb1fb114e0655d3
Rajwinder Bhatoe <rajwinder.bhatoe@gmail.com>
2021-11-25 15:06:23 +0000

    Completed search and add favourites

--- favouritesCLI.ts
+++ favouritesCLI.ts
@@ -13,62 +13,91 @@ interface Movie {
 }
 
 async function mainMenu() {
-  console.log("Welcome to search-movies-cli! \n");
+  console.log("Welcome to search-movies-cli!");
   let quit = false;
   while (!quit) {
     const actions = ["Search", "See Favourites", "Quit"];
-    // console.log(
-    //   "\n",
-    //   "[1] Search \n",
-    //   "[2] See Favourites \n",
-    //   "[3] Quit \n",
-    //   "\n"
-    // );
-    let action = await keyInSelect(actions, "Choose an action! \n");
-    if (action === 1) {
-      let movies = await searchMovies();
-      await addFavourite(movies);
-    } else if (action === 2) {
+    let action = await keyInSelect(actions, "Choose an action! \n", {
+      cancel: false,
+    });
+    if (action === 0) {
+      let moviesAndIds = await searchMovies();
+      await addFavourite(moviesAndIds);
+    } else if (action === 1) {
       await seeFavourites();
-    } else if (action === 3) {
+    } else if (action === 2) {
       console.log("All done!");
       quit = true;
     } else {
-      action = await keyInSelect(actions, "Choose an action! \n");
+      action = await keyInSelect(actions, "Choose an action! \n", {
+        cancel: false,
+      });
     }
   }
 }
 
-async function searchMovies(): Promise<Movie[]> {
-  console.log("search");
+async function searchMovies(): Promise<string[][]> {
   const client = new Client({ database: "omdb" });
   await client.connect();
   let searchTerm = await question("Search term: ");
-  let movies: Movie[];
+  let movies: string[];
+  let moviesIds: string[];
   try {
     // try this
     const text =
-      "SELECT id, name, date, runtime, budget, revenue, vote_average, votes_count FROM movies WHERE LOWER(name) LIKE LOWER($1) ORDER BY date DESC LIMIT 10;";
-    const values = [`%${searchTerm}%`];
+      "SELECT id, name, date, runtime, budget, revenue, vote_average, votes_count FROM movies WHERE LOWER(name) LIKE LOWER($1) AND kind=$2 ORDER BY date DESC LIMIT 9;";
+    const values = [`%${searchTerm}%`, "movie"];
     const res = await client.query(text, values);
     console.table(res.rows);
-    movies = res.rows;
+    movies = res.rows.map((field) => field.name);
+    moviesIds = res.rows.map((field) => field.id);
   } catch (e) {
     // console error
     console.error(e.stack);
   } finally {
     await client.end();
   }
-  return movies;
+  return [movies, moviesIds];
 }
 
-async function addFavourite(movies: Movie[]): Promise<void> {
-  console.log("movies \n", movies);
-  console.log("add fav");
+async function addFavourite(moviesAndIds: string[][]): Promise<void> {
+  const favourite = await keyInSelect(
+    moviesAndIds[0],
+    "Select a movie to favourite "
+  );
+  if (favourite !== -1) {
+    const client = new Client({ database: "omdb" });
+    await client.connect();
+    try {
+      const text = "INSERT INTO favourites (movie_id) VALUES ($1)";
+      const values = [`${parseInt(moviesAndIds[1][favourite])}`];
+      await client.query(text, values);
+      console.log(`Saving favourite movie: ${moviesAndIds[0][favourite]}`);
+    } catch (e) {
+      // console error
+      console.error(e.stack);
+    } finally {
+      await client.end();
+    }
+  } else {
+    console.log("Cancelled");
+  }
 }
 
 async function seeFavourites(): Promise<void> {
-  console.log("see favs");
+  const client = new Client({ database: "omdb" });
+  await client.connect();
+  try {
+    const text =
+      "SELECT m.id, m.name, m.date, m.runtime, m.budget, m.revenue, m.vote_average, m.votes_count FROM movies m JOIN favourites f ON m.id = f.movie_id;";
+    const res = await client.query(text);
+    console.table(res.rows);
+  } catch (e) {
+    // console error
+    console.error(e.stack);
+  } finally {
+    await client.end();
+  }
 }
 
 mainMenu();
